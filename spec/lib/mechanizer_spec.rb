8d635859316817c32e92dfe7dd79ca296f3da4e1
Joshua Szmajda <josh@optoro.com>
2011-07-09 17:51:47 -0400

    Had accidentally left some evil, evil tabs instead of spaces.

--- spec/lib/mechanizer_spec.rb
+++ spec/lib/mechanizer_spec.rb
@@ -1,39 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Mechanizer" do
-	before :all do
-		mock_seller_central_page_results!
-	end
+  before :all do
+    mock_seller_central_page_results!
+  end
 
-	it "retains access to the last page loaded" do
-		mech = AmazonSellerCentral.mechanizer
-		mech.login_to_seller_central
-		mech.last_page.body.should =~ /Welcome! You are signed in as/
-	end
+  it "retains access to the last page loaded" do
+    mech = AmazonSellerCentral.mechanizer
+    mech.login_to_seller_central
+    mech.last_page.body.should =~ /Welcome! You are signed in as/
+  end
 
-	it "logs in and returns the home page" do
-		mech = AmazonSellerCentral.mechanizer
-		mech.login_to_seller_central
-	end
+  it "logs in and returns the home page" do
+    mech = AmazonSellerCentral.mechanizer
+    mech.login_to_seller_central
+  end
 
   it "raises a LinkNotFoundError if the requested link doesn't exist" do
-		mech = AmazonSellerCentral.mechanizer
-		mech.login_to_seller_central
+    mech = AmazonSellerCentral.mechanizer
+    mech.login_to_seller_central
     lambda {
       mech.follow_link_with(:text => "Foo")
     }.should raise_exception(AmazonSellerCentral::Mechanizer::LinkNotFoundError)
   end
 
   it "resets to a nil agent" do
-		mech = AmazonSellerCentral.mechanizer
-		mech.login_to_seller_central
+    mech = AmazonSellerCentral.mechanizer
+    mech.login_to_seller_central
     lambda {
       mech.follow_link_with(:text => "Feedback")
     }.should_not raise_exception
     AmazonSellerCentral.mechanizer.reset!
 
-		mech = AmazonSellerCentral.mechanizer
-		mech.login_to_seller_central
+    mech = AmazonSellerCentral.mechanizer
+    mech.login_to_seller_central
     AmazonSellerCentral.mechanizer.reset!
     lambda {
       mech.follow_link_with(:text => "Feedback")
--- spec/support/sample_pages.rb
+++ spec/support/sample_pages.rb
@@ -1,18 +1,18 @@
 require 'fakeweb'
 def mock_seller_central_page_results!
-	mock_pages = {}
-	Dir.glob("#{File.join(File.dirname(__FILE__), 'sample_pages')}/*.html").each do |sample_html|
-		page_name = sample_html.split(/\//).last.gsub(/\.html$/,'').downcase.tr_s(' ', '_').to_sym
-		mock_pages[page_name] = File.read(sample_html)
-	end
+  mock_pages = {}
+  Dir.glob("#{File.join(File.dirname(__FILE__), 'sample_pages')}/*.html").each do |sample_html|
+    page_name = sample_html.split(/\//).last.gsub(/\.html$/,'').downcase.tr_s(' ', '_').to_sym
+    mock_pages[page_name] = File.read(sample_html)
+  end
 
-	FakeWeb.allow_net_connect = false
-	FakeWeb.register_uri(:any, 'https://sellercentral.amazon.com/', :response => mock_pages[:seller_central])
-	FakeWeb.register_uri(:any, 'https://sellercentral.amazon.com/gp/homepage.html', :response => mock_pages[:seller_central_redirect])
-	FakeWeb.register_uri(:post, 'https://sellercentral.amazon.com/gp/sign-in/sign-in.html', :response => mock_pages[:seller_central_homepage])
+  FakeWeb.allow_net_connect = false
+  FakeWeb.register_uri(:any, 'https://sellercentral.amazon.com/', :response => mock_pages[:seller_central])
+  FakeWeb.register_uri(:any, 'https://sellercentral.amazon.com/gp/homepage.html', :response => mock_pages[:seller_central_redirect])
+  FakeWeb.register_uri(:post, 'https://sellercentral.amazon.com/gp/sign-in/sign-in.html', :response => mock_pages[:seller_central_homepage])
 
-	FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/gp/feedback-manager/home.html/ref=ag_feedback_mmap_home', :response => mock_pages[:feedback_manager])
-	FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/gp/feedback-manager/view-all-feedback.html?ie=UTF8&sortType=sortByDate&dateRange=&descendingOrder=1', :response => mock_pages[:feedback_page_1])
+  FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/gp/feedback-manager/home.html/ref=ag_feedback_mmap_home', :response => mock_pages[:feedback_manager])
+  FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/gp/feedback-manager/view-all-feedback.html?ie=UTF8&sortType=sortByDate&dateRange=&descendingOrder=1', :response => mock_pages[:feedback_page_1])
   FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/gp/feedback-manager/view-all-feedback.html?ie=UTF8&sortType=sortByDate&pageSize=50&dateRange=&currentPage=2&descendingOrder=1', :response => mock_pages[:feedback_page_2])
   FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/gp/feedback-manager/view-all-feedback.html?ie=UTF8&sortType=sortByDate&pageSize=50&dateRange=&currentPage=3&descendingOrder=1', :response => mock_pages[:feedback_page_last])
 
