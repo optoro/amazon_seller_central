c77337a4aaaa33a1c7a6bb44b5161f452f279daa
Yasha Portnoy <yportnoy@optoro.com>
2012-05-14 17:47:46 -0400

    Sample Payment Pages

---
+++ spec/lib/payments_page_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "PaymentsPage" do
+  describe "available_report_ids" do
+    it "should return an array of report ids that are available for you to download from seeler central" do
+      report_ids = AmazonSellerCentral::PaymentsPage.available_report_ids
+      report_ids.class.should == Array
+      report_ids.count.should == 10
+      report_ids[4].should == "5917406303"
+    end
+  end
+end
