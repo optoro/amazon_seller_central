112d83c7a9244ea19e9d80e7fc36e3d230c66836
Yasha Portnoy <yportnoy@optoro.com>
2012-07-24 16:26:20 -0400

    Typo

--- lib/amazon_seller_central/payments_page.rb
+++ lib/amazon_seller_central/payments_page.rb
@@ -21,7 +21,7 @@ module AmazonSellerCentral
       mech = AmazonSellerCentral.mechanizer
       mech.login_to_seller_central
       mech.follow_link_with(:text => "Payments")
-      payments_page = mech.follow_link_with(:text => "All Settlements")
+      payments_page = mech.follow_link_with(:text => "All Statements")
       @page = PaymentsPage.new(:page => payments_page, :agent => mech)
       id_array << @page.page.links_with(:text => "Download Flat File").map{|link| link.href.match(/_GET_FLAT_FILE_PAYMENT_SETTLEMENT_DATA__(\d+)\.txt/)[1]}
       while(@page.has_next?)
