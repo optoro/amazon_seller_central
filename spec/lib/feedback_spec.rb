834ed946fcb3d77d5b943ed47be04a147e6f770c
Justin Shakespear <jshakespear@gmail.com>
2013-04-22 12:03:54 -0600

    changed tests per comments on pull request

--- spec/lib/feedback_spec.rb
+++ spec/lib/feedback_spec.rb
@@ -11,13 +11,17 @@ describe "Feedback" do
     end
   end
 
-  it "should have a valid date format of: YYYY-mm-dd HH:MM:SS -0T00" do
-    stream = File.read(File.expand_path(File.dirname(__FILE__) + '/../support/sample_pages/Feedback Page 1.html'))
-    FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/feedback/', :body => stream, :content_type => 'text/html')
-    agent = Mechanize.new
-    page = agent.get('https://sellercentral.amazon.com/feedback/')
-    fp = AmazonSellerCentral::FeedbackPage.new(:page => page, :agent => agent)
+  it "should parse the date correctly" do
+    #stream = File.read(File.expand_path(File.dirname(__FILE__) + '/../support/sample_pages/Feedback Page 1.html'))
+    #FakeWeb.register_uri(:get, 'https://sellercentral.amazon.com/feedback/', :body => stream, :content_type => 'text/html')
+    #agent = Mechanize.new
+    #page = agent.get('https://sellercentral.amazon.com/feedback/')
+    #fp = AmazonSellerCentral::FeedbackPage.new(:page => page, :agent => agent)
+    fp = AmazonSellerCentral::FeedbackPage.load_first_page
     f = fp.parse
-    f.first.date.to_s.should == '2011-07-03 00:00:00 -0600'
+    feedback_date = f.first.date
+    feedback_date.year.should == 2011
+    feedback_date.month.should == 7
+    feedback_date.day.should == 3
   end
 end
