3cab932ff45a5ad7b9a35be5fa36bebc040969fb
Joshua Szmajda <josh@optoro.com>
2014-10-07 15:32:13 -0400

    removed ruby-debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'faker'
 require 'amazon_seller_central'
-require 'ruby-debug'
-
-if Debugger.respond_to?(:settings)
-  Debugger.settings[:autoeval] = true
-  Debugger.settings[:autolist] = 1
-end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
